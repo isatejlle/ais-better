9bfec5ae3e03ffb99cf5b0b40cfe5fb5a0e427ab
James Escobedo <escobedo.james01@gmail.com>
2024-07-18 20:30:31 -0400

    PRAISE JESUS

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ document.getElementById('predictButton').addEventListener('click', async () => {
   formData.append('file', imageInput.files[0]);
 
   try {
-      const response = await fetch('http://localhost:8000/predict', {
+      const response = await fetch('https://ai-scholars-2024.de.r.appspot.com/predict/', {
           method: 'POST',
           body: formData,
       });
